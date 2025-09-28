70aa68f3bf8c2c72e0d92f7dd9520b48d2762f94
rijwanrk <rijwankhan.rkn@gmail.com>
2025-09-28 08:01:39 +0530

    update url

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -16,7 +16,7 @@ const Login = ({ onSubmit, onSwitchMode }) => {
   const [rememberMe, setRememberMe] = useState(false)
   const [loading, setLoading] = useState(false)
   const navigate = useNavigate()
-  const url = "http://localhost:4000"
+  const url = "https://task-management-system-backend-cceq.onrender.com"
 
   // Auto-login
   useEffect(() => {
