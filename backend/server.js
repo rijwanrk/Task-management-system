23be89310f6937bc6aaf3a794da2ae4450a170e6
rijwanrk <rijwankhan.rkn@gmail.com>
2025-09-28 09:04:00 +0530

    server update

--- backend/server.js
+++ backend/server.js
@@ -13,11 +13,11 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 // Middleware
-app.use(cors({
-  origin: 'https://task-management-system-backend-cceq.onrender.com', // Vercel का URL
-  credentials: true
-}));
-// app.use(cors());
+// app.use(cors({
+//   origin: 'https://task-management-system-backend-cceq.onrender.com', // Vercel का URL
+//   credentials: true
+// }));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
