153b28ddd5f037f9cc83de9018d29453f222935c
rijwanrk <rijwankhan.rkn@gmail.com>
2025-09-28 08:56:54 +0530

    some changes

--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,22 @@ import { connectDB } from './config/db.js';
 
 import userRouter from './routes/userRoute.js';
 import taskRouter from './routes/taskRoute.js';
+import cors from 'cors';
+
+
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://task-management-system-backend-cceq.onrender.com', // Vercel का URL
+  credentials: true
+}));
+// app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 
 // Database Connection
 connectDB();
